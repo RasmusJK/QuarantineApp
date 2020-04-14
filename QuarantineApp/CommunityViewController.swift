1bc60c3a406adbf4b0dad35e0d95de5139094413
tiiatoiv <tiia.toivonen@metropolia.fi>
2020-04-14 18:35:37 +0300

    tab bar navigation & viewcontrollers added

---
+++ QuarantineApp/CommunityViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CommunityViewController.swift
+//  QuarantineApp
+//
+//  Created by iosdev on 14.4.2020.
+//  Copyright © 2020 Roope Vaarama. All rights reserved.
+//
+
+import UIKit
+
+class CommunityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
