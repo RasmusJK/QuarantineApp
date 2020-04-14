35139f23e37f7502caa5a905c23622d09a108214
RoopeVaarama <roope.vaarama@gmail.com>
2020-04-14 13:35:16 +0300

    Initial Commit

---
+++ QuarantineApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuarantineApp
+//
+//  Created by Roope Vaarama on 14.4.2020.
+//  Copyright © 2020 Roope Vaarama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
