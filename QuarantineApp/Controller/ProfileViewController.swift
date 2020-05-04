3257aac23b804e5bb555156a93610c375930fd6b
Rasmus Karling <rasmus.karling@gmail.com>
2020-05-04 18:01:36 +0300

    trying to add own reviews in profile

--- QuarantineApp/Controller/ProfileViewController.swift
+++ QuarantineApp/Controller/ProfileViewController.swift
@@ -11,13 +11,24 @@ import Firebase
 import FirebaseAuth
 import FirebaseFirestore
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let db = Firestore.firestore()
+    var userReviews = [UserReview]()
+    var list = [String: Any]()
+    var reviewArray = [String: Any]()
     
     @IBOutlet weak var usernameLabel: UILabel!
+    
     @IBOutlet weak var profileTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        profileTableView.delegate = self
+        profileTableView.dataSource = self
+      
+       getData()
+       
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -27,7 +38,26 @@ class ProfileViewController: UIViewController {
             performSegue(withIdentifier: "toAuth", sender: nil)
         }
     }
+    //MARK: TableviewDataSource
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return userReviews.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileCell", for: indexPath) as? ProfileTableViewCell else {
+            fatalError("rip")
+        }
+        
+        let myRev = userReviews[indexPath.row]
+        
+        cell.profileTitleLabel.text = myRev.title
+        cell.profileTextLabel.text = myRev.review
+    
+        
+        return cell
+    }
     
     /*
      // MARK: - Navigation
@@ -38,6 +68,46 @@ class ProfileViewController: UIViewController {
      // Pass the selected object to the new view controller.
      }
      */
+    func getData() {
+        
+        db.collection("reviews").whereField("reviewUser", isEqualTo: "testuser").getDocuments() {(querySnapshot,err)  in
+           
+            if let err = err {
+                print("error getting documents \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                   // print(document.data())
+                    let data = document.data()
+                    self.reviewArray = data
+                    
+                    print("reviewArray", self.reviewArray)
+                    
+                            var movietitle = ""
+                            var movierating = ""
+                            var reviewtext = ""
+                            var username = ""
+                            var reviewCategory = ""
+                    
+                        for (key, value) in self.list {
+                                print(" single value and key:\(key) \(value)")
+                                
+                            movietitle = self.list["reviewItem"] as! String
+                            movierating = self.list["reviewRating"] as! String
+                            reviewtext = self.list["reviewText"] as! String
+                            username = self.list["reviewUser"] as! String
+                            reviewCategory = self.list["reviewCategory"] as? String ?? "no category"
+                            }
+                     let object = UserReview(title: movietitle, rating: movierating, username: username, review: reviewtext, category: reviewCategory) ?? UserReview(title: "no value", rating: "no value", username: "no value", review: "no value", category: "no value")!
+                    
+                    self.userReviews.append(object ?? UserReview(title: "no value", rating: "no value", username: "no value", review: "no value", category: "no value")! )
+                    
+                    self.profileTableView.reloadData()
+
+                }
+            }
+        }
+    }
+    
     
     @IBAction func logoutButton(_ sender: UIButton) {
         //Logout from firebase
@@ -49,5 +119,6 @@ class ProfileViewController: UIViewController {
         print("Auth: Logged out")
 
     }
+    
     
 }
