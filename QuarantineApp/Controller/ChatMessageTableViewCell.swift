da4510a1c3d33ef077da5ae4a5b0f973d4cb0eb9
tiiatoiv <tiia.toivonen@metropolia.fi>
2020-05-04 18:08:56 +0300

    start for chat functionality

---
+++ QuarantineApp/Controller/ChatMessageTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ChatMessageTableViewCell.swift
+//  QuarantineApp
+//
+//  Created by iosdev on 4.5.2020.
+//  Copyright © 2020 Roope Vaarama. All rights reserved.
+//
+
+import UIKit
+
+class ChatMessageTableViewCell: UITableViewCell {
+    @IBOutlet weak var chatMessageLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
