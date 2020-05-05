bf5b3f4585a5e334b83e85312fc18e7148099c49
Ville Liekari <ville.liekari@metropolia.fi>
2020-05-05 14:48:42 +0300

    login condition for reviews/chat, chat sorting and realtime updates

--- QuarantineApp/Controller/ChatMessageTableViewController.swift
+++ QuarantineApp/Controller/ChatMessageTableViewController.swift
@@ -31,7 +31,7 @@ class ChatMessageTableViewController: UITableViewController {
     var finallist = [String: Any]()
     var chatMessages = [ChatMessage]()
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -42,69 +42,72 @@ class ChatMessageTableViewController: UITableViewController {
         }
         print("as string: \(currentCategory)")
         
-        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
         //Calls a function that fetches the user reviews from database
         downloadUserReviewsNow() { reviewArray, error in
-        if let error = error {
-           return
-        }
+            if error != nil {
+                return
+            }
             self.list = reviewArray
             print("this is the dictionary object \(reviewArray)")
-        
+            
             var chatRoom = ""
             var chatUsername = ""
             var chatMessage = ""
-    
-        for (key, value) in self.list {
+            
+            for (key, value) in self.list {
                 print(" single value and key:\(key) \(value)")
                 
-            chatRoom = self.list["chatRoom"] as! String
-            chatUsername = self.list["chatUser"] as! String
-            chatMessage = self.list["chatText"] as! String
+                chatRoom = self.list["chatRoom"] as! String
+                chatUsername = self.list["chatUser"] as! String
+                chatMessage = self.list["chatText"] as! String
             }
             
-        let object = ChatMessage(chatRoom: chatRoom, chatUsername: chatUsername, chatMessage: chatMessage) ?? ChatMessage(chatRoom: "No Room", chatUsername: "no user", chatMessage: "no message")!
+            let object = ChatMessage(chatRoom: chatRoom, chatUsername: chatUsername, chatMessage: chatMessage)
             
-            if object.chatRoom == self.selectedCity {
-                self.chatMessages.append(object ?? ChatMessage(chatRoom: "No Room", chatUsername: "no user", chatMessage: "no message")!)
+            if object?.chatRoom == self.selectedCity {
+                self.chatMessages.append(object!)
             }
-        self.tableView.reloadData()
+            self.tableView.reloadData()
+            self.scrollToBottom()
         }
     }
-
+    
     
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return chatMessages.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cellIdentifier = "messageCell"
         guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? ChatMessageTableViewCell else {
             fatalError("fucked up loading data to userreview")
         }
-            
+        
         let userRev = chatMessages[indexPath.row]
-
+        
         cell.chatMessageLabel.text = userRev.chatMessage
         cell.usernameLabel.text = userRev.chatUsername
- 
-    //    let userRev = String(describing: list[indexPath.row])
-      //  print("for the cell: \(userRev)")
-      //  cell.reviewTextLabel.text = userRev
+        
+        //    let userRev = String(describing: list[indexPath.row])
+        //  print("for the cell: \(userRev)")
+        //  cell.reviewTextLabel.text = userRev
         // Configure the cell...
-
+        
         return cell
     }
     
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
@@ -113,22 +116,31 @@ class ChatMessageTableViewController: UITableViewController {
     
     
     //MARK: Private functions
-
+    
     //Function for fetching user reviews from database that's called on the viewDidLoad
     func downloadUserReviewsNow(completion: @escaping ([String: Any], Error?) -> Void) {
         var reviewArray = [String: Any]()
-        db.collection("chat").getDocuments { QuerySnapshot, error in
-          if let error = error {
-            print(error)
-            completion(reviewArray, error)
-            return
-          }
+        db.collection("chat").addSnapshotListener { QuerySnapshot, error in
+            if let error = error {
+                print(error)
+                completion(reviewArray, error)
+                return
+            }
             
-          for doc in QuerySnapshot!.documents {
-            let data = doc.data()
-            reviewArray = data
-            
-            completion(reviewArray, error)
+            for doc in QuerySnapshot!.documents {
+                let data = doc.data()
+                reviewArray = data
+                
+                completion(reviewArray, error)
+            }
+        }
+    }
+    
+    func scrollToBottom(){
+        if chatMessages.count > 5 {
+            DispatchQueue.main.async {
+                let indexPath = IndexPath(row: self.chatMessages.count-1, section: 0)
+                self.tableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
             }
         }
     }
@@ -140,20 +152,20 @@ class ChatMessageTableViewController: UITableViewController {
     @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
         
         if let sourceViewController = sender.source as? CreateChatMessageViewController, let newMessage = sourceViewController.message{
-
-                //Add a new review
-                let newIndexPath = IndexPath(row: chatMessages.count, section: 0)
-                let newMessageAsString = String(describing: "\(newMessage)")
             
-                chatMessages.append(newMessage)
-                tableView.insertRows(at: [newIndexPath], with: .automatic)
+            //Add a new review
+            let newIndexPath = IndexPath(row: chatMessages.count, section: 0)
+            let newMessageAsString = String(describing: "\(newMessage)")
             
-           downloadUserReviewsNow() { reviewArray, error in
-            if let error = error {
-               return
-            }
-              self.list = reviewArray
-              self.tableView.reloadData()
+            chatMessages.append(newMessage)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+            
+            downloadUserReviewsNow() { reviewArray, error in
+                if error != nil {
+                    return
+                }
+                self.list = reviewArray
+                self.tableView.reloadData()
             }
         }
     }
